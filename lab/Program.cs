479ba30bffd45fa825a3e9ff24d04af51796c4a9
shockfox <shockfox@209-4>
2024-10-04 14:54:57 +0200

    Добавьте файлы проекта.

---
+++ lab/Program.cs
@@ -0,0 +1,34 @@
+﻿try
+{
+    Console.Write("Введите число (1 до 99): ");
+    double x = double.Parse(Console.ReadLine());
+
+    if (x < 1 || x > 99)
+    {
+        Console.WriteLine("Я же сказал вести число от 1 до 99.");
+        return;
+    }
+
+    string z;
+    double y = x % 10;
+
+    if (y == 1 && x != 11)
+    {
+        z = "копейка";
+    }
+    else if (y >= 2 && y <= 4 && (x < 10 || x >= 20))
+    {
+        z = "копейки";
+    }
+    else
+    {
+        z = "копеек";
+    }
+
+    Console.WriteLine($"{x} {z}");
+}
+
+catch
+{
+    Console.WriteLine("не правильное значение ");
+}
